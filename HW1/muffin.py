e6be36108a148aa00a358ac1f5eda4937a362ba9
zhenmunse <2711675247@qq.com>
2025-10-04 19:23:41 -0700

    Update HW1

--- HW1/muffin.py
+++ HW1/muffin.py
@@ -7,7 +7,7 @@ class Muffin:
     def __str__(self):
         return self.flavor
     def bake_muffin(self):
-        if self.current_level < len(Muffin.cook_levels) - 1: # Prevent going out of bounds
+        if self.current_level < len(self.cook_levels) - 1: # Prevent going out of bounds
             self.current_level += 1 # Increment cooking level
     def get_description(self):
-        return f"{Muffin.cook_levels[self.current_level]} {self.flavor} muffin priced at ${self.price:.2f}" # Formatted price
+        return f"{self.cook_levels[self.current_level]} {self.flavor} muffin priced at ${self.price:.2f}" # Formatted price
